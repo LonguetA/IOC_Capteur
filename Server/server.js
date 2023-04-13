01086cd1572453d72ff0ad5b13cc44e0ea9c83aa
LonguetA <65488503+LonguetA@users.noreply.github.com>
2023-04-13 15:52:59 +0200

    push

--- Server/server.js
+++ Server/server.js
@@ -16,14 +16,16 @@ app.get('/',(req,res) => {
                         <meta charset="uft-8"/>
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
                         <title>IOC</title>
+			<link rel="stylesheet" href="style.css">		
 			<script>
         			function clickFn(event) {
           				const checkbox = event.currentTarget;
 					const led = document.getElementById("ledToggle");
           				led.value = checkbox.checked ? 'LED ON' : 'LED OFF';
-          				led.closest('form').submit()
+					led.closest('form').submit()          				
+					//document.getElementById("btn").click()
         			}
-      			</script>	
+			</script>	
                 </head>
                 <body>
                         <h1>IOC</h1>
@@ -36,25 +38,25 @@ app.get('/',(req,res) => {
                                 <input name="send"  type="submit" value="Submit">
                         </form>
 
-                        <form method="GET" action="/LED">
-                                <input name="send"  type="submit" value="LED ON">
-                        </form>
-
-                        <form method="GET" action="/LED">
-                                <input name="send"  type="submit" value="LED OFF">
-                        </form>
-        				<label class="switch">
-          				<input type="checkbox" checked=true name="checkbox" value="dark" onclick="clickFn(event)"> 
+                        <div>
+			LED : 
+        			<label class="switch">
+          				<input type="checkbox" name="checkbox" value="dark" onclick="clickFn(event)"> 
           				<span class="slider round"></span>
         			</label>
-
+			</div>
 			<form method="post" action="/LED">
                 		<input type="hidden" id="ledToggle" name="led" value="test">
-			</form>
+				</form>
 		</body>
                 </html>
                 `);
 });
+
+app.get('/style.css',(req,res) => {
+	res.sendFile(__dirname + "/" + "style.css");
+});
+
 app.get('/LUM',(req,res) => {
 
   const lum = readFileSync('../lum1.txt','utf-8');
@@ -106,7 +108,7 @@ const pythonProcess = spawn('python3', [scriptPath,'esp1/lcd','LCD '+ req.query.
 app.post('/LED',(req,res) => {
 
         console.log(req.body.led);
-
+	//console.log(req);
        // var spawn = require("child_process").spawn;
        // var process = spawn('python3',["../sendMQTT.py"]);
 	
@@ -137,7 +139,7 @@ pythonProcess.stderr.on('data', (data) => {
 pythonProcess.on('close', (code) => {
   console.log(`Processus Python terminé avec le code de sortie ${code}`);
 });
-
+	/*res.send(
         res.send(`<!DOCTYPE html>
         <html lang=fr>
         <head>
@@ -149,6 +151,95 @@ pythonProcess.on('close', (code) => {
                 LED PRINT : ${req.body.led}
         </body>
         </html>`)
+	*/
+	console.log(req.body.led == "LED ON");
+	if (req.body.led == "LED ON"){
+		res.send(`
+                <!DOCTYPE html>
+                <html lang=fr>
+                <head>
+                        <meta charset="uft-8"/>
+                        <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        <title>IOC</title>
+                        <link rel="stylesheet" href="style.css">                
+                        <script>
+                                function clickFn(event) {
+                                        const checkbox = event.currentTarget;
+                                        const led = document.getElementById("ledToggle");
+                                        led.value = checkbox.checked ? 'LED ON' : 'LED OFF';
+                                        led.closest('form').submit()
+                                }
+                        </script>       
+                </head>
+                <body>
+                        <h1>IOC</h1>
+                        <form method="GET" action="/LUM">
+                          <button id='button'>LUM</button>
+                        </form>
+
+                        <form methode="POST" action="/LCD">
+                                <input id="team_name" name="lcd" type="text" value="LCD">
+                                <input name="send"  type="submit" value="Submit">
+                        </form>
+
+                        <div>LED : 
+                                        <label class="switch">
+                                        <input type="checkbox" checked  name="checkbox" value="dark" onclick="clickFn(event)"> 
+                                        <span class="slider round"></span>
+                                </label>
+			</div>
+                        <form method="post" action="/LED">
+                                <input type="hidden" id="ledToggle" name="led" value="test">
+                        </form>
+                </body>
+                </html>
+                `);
+	}
+	else {
+
+		res.send(`
+                <!DOCTYPE html>
+                <html lang=fr>
+                <head>
+                        <meta charset="uft-8"/>
+                        <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        <title>IOC</title>
+                        <link rel="stylesheet" href="style.css">                
+                        <script>
+                                function clickFn(event) {
+                                        const checkbox = event.currentTarget;
+                                        const led = document.getElementById("ledToggle");
+                                        led.value = checkbox.checked ? 'LED ON' : 'LED OFF';
+                                        led.closest('form').submit()
+                                }
+                        </script>       
+                </head>
+                <body>
+                        <h1>IOC</h1>
+                        <form method="GET" action="/LUM">
+                          <button id='button'>LUM</button>
+                        </form>
+
+                        <form methode="POST" action="/LCD">
+                                <input id="team_name" name="lcd" type="text" value="LCD">
+                                <input name="send"  type="submit" value="Submit">
+                        </form>
+
+                        <div> LED : 
+                                        <label class="switch">
+                                        <input type="checkbox" name="checkbox" value="dark" onclick="clickFn(event)"> 
+                                        <span class="slider round"></span>
+                                </label>
+			</div>
+                        <form method="post" action="/LED">
+                                <input type="hidden" id="ledToggle" name="led" value="test">
+                        </form>
+                </body>
+                </html>
+                `);
+	}
+	
+	res.status(200);
 })
 
-app.listen(8000,() => console.log('172.20.10.5'));
+app.listen(8000,() => console.log('172.20.10.2'));
